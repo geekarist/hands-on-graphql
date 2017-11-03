59fa870c533b3b5a76cf0716d7246936f14a6383
tsimonnet@xebia.fr <tsimonnet@xebia.fr>
2017-11-03 01:21:14 +0100

    Add question numbers in TODOs

--- src/schemas/root.js
+++ src/schemas/root.js
@@ -5,14 +5,14 @@ const {
   GraphQLSchema,
 } = require("graphql");
 
-// TODO définir le type User et les resolveurs pour ses attributs
+// TODO 3. : définir le type User et les resolveurs pour ses attributs
 const UserType;
 
-// TODO définir le type Query et les résolveurs pour ses attributs
+// TODO 2. : définir le type Query et les résolveurs pour ses attributs
 const QueryType;
 
 
-// TODO définir le schéma de l'application (contient uniquement les deux ressources query et mutation)
+// TODO 1. : définir le schéma de l'application (contient uniquement les deux ressources query et mutation)
 const rootSchema;
 
 module.exports = rootSchema;
