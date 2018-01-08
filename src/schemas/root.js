2a298ccd5fd912b9940ac16ced4c7308603a4231
cpcp <geekarist@users.noreply.github.com>
2018-01-08 11:21:05 +0100

    Complete question 1

--- src/schemas/root.js
+++ src/schemas/root.js
@@ -6,13 +6,35 @@ const {
 } = require("graphql");
 
 // TODO 3. : définir le type User et les resolveurs pour ses attributs
-const UserType;
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: {
+      type: GraphQLInt,
+      resolve: () => 1
+    },
+    name: {
+      type: GraphQLString,
+      resolve: () => "toto"
+    }
+  })
+});
 
 // TODO 2. : définir le type Query et les résolveurs pour ses attributs
-const QueryType;
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    user: {
+      type: UserType,
+      resolve: () => ""
+    },
+  })
+});
 
 
 // TODO 1. : définir le schéma de l'application (contient uniquement les deux ressources query et mutation)
-const rootSchema;
+const rootSchema = new GraphQLSchema({
+  query: QueryType
+});
 
 module.exports = rootSchema;
